02e537c0312b760a1203feff51635fea13ee8436
BhaveerH <2116470@students.wits.ac.za>
2022-06-03 17:37:32 +0200

    HTML, CSS & JS Iteration 15
    links changed from HTML to JS event listeners for blog and design pages.

--- myWebsite/blog.js
+++ myWebsite/blog.js
@@ -1,7 +1,14 @@
 const btnDesign = document.getElementById('linkDesign');
 const btnIndex = document.getElementById('linkIndex');
 const btnPort = document.getElementById('linkPort');
-
+const bLinkOne = document.getElementById('linkOne');
+const bLinkTwo = document.getElementById('linkTwo');
+const bLinkThree = document.getElementById('linkThree');
+const bLinkFour = document.getElementById('linkFour');
+const bLinkFive = document.getElementById('linkFive');
+const bLinkSix = document.getElementById('linkSix');
+const bLinkSeven = document.getElementById('linkSeven');
+const bLinkEight = document.getElementById('linkEight');
 
 function designLinkClick() {
   window.open("design.html", "_blank");
@@ -15,6 +22,46 @@ function portLinkClick() {
   window.open("portfolio.html", "_blank");
 }
 
+function bLinkOneClick() {
+  window.open("blog.html", "_self");
+}
+
+function bLinkTwoClick() {
+  window.open("blog2.html", "_self");
+}
+
+function bLinkThreeClick() {
+  window.open("blog3.html", "_self");
+}
+
+function bLinkFourClick() {
+  window.open("blog4.html", "_self");
+}
+
+function bLinkFiveClick() {
+  window.open("blog5.html", "_self");
+}
+
+function bLinkSixClick() {
+  window.open("blog6.html", "_self");
+}
+
+function bLinkSevenClick() {
+  window.open("blog7.html", "_self");
+}
+
+function bLinkEightClick() {
+  window.open("blog8.html", "_self");
+}
+
 btnDesign.addEventListener('click', designLinkClick);
 btnIndex.addEventListener('click', homeLinkClick);
 btnPort.addEventListener('click', portLinkClick);
+bLinkOne.addEventListener('click', bLinkOneClick);
+bLinkTwo.addEventListener('click', bLinkTwoClick);
+bLinkThree.addEventListener('click',bLinkThreeClick);
+bLinkFour.addEventListener('click', bLinkFourClick);
+bLinkFive.addEventListener('click', bLinkFiveClick);
+bLinkSix.addEventListener('click', bLinkSixClick);
+bLinkSeven.addEventListener('click', bLinkSevenClick);
+bLinkEight.addEventListener('click', bLinkEightClick);
--- myWebsite/design.js
+++ myWebsite/design.js
@@ -1,6 +1,13 @@
 const btnBlog = document.getElementById('linkBlog');
 const btnIndex = document.getElementById('linkIndex');
 const btnPort = document.getElementById('linkPort');
+const dLinkOne = document.getElementById('linkOne');
+const dLinkTwo = document.getElementById('linkTwo');
+const dLinkThree = document.getElementById('linkThree');
+const dLinkFour = document.getElementById('linkFour');
+const dLinkFive = document.getElementById('linkFive');
+const dLinkSix = document.getElementById('linkSix');
+const dLinkSeven = document.getElementById('linkSeven');
 
 
 function blogLinkClick() {
@@ -15,6 +22,42 @@ function portLinkClick() {
   window.open("portfolio.html", "_blank");
 }
 
+function dLinkOneClick() {
+  window.open("design.html", "_self");
+}
+
+function dLinkTwoClick() {
+  window.open("design2.html", "_self");
+}
+
+function dLinkThreeClick() {
+  window.open("design3.html", "_self");
+}
+
+function dLinkFourClick() {
+  window.open("design4.html", "_self");
+}
+
+function dLinkFiveClick() {
+  window.open("design5.html", "_self");
+}
+
+function dLinkSixClick() {
+  window.open("design6.html", "_self");
+}
+
+function dLinkSevenClick() {
+  window.open("design7.html", "_self");
+}
+
+
 btnBlog.addEventListener('click', blogLinkClick);
 btnIndex.addEventListener('click', homeLinkClick);
 btnPort.addEventListener('click', portLinkClick);
+dLinkOne.addEventListener('click', dLinkOneClick);
+dLinkTwo.addEventListener('click', dLinkTwoClick);
+dLinkThree.addEventListener('click',dLinkThreeClick);
+dLinkFour.addEventListener('click', dLinkFourClick);
+dLinkFive.addEventListener('click', dLinkFiveClick);
+dLinkSix.addEventListener('click', dLinkSixClick);
+dLinkSeven.addEventListener('click', dLinkSevenClick);
